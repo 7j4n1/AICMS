5b0698b0279917aff9c7908798b30a4a562181fd
7j4n1 <ahmaholy@gmail.com>
2024-04-29 10:48:39 +0100

    Add user creation and role assignment in DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +14,19 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        $user = \App\Models\Admin::where('username', 'superadmin')->first();
 
-        \App\Models\Admin::factory()->create()->assignRole('super-admin');
+        if(!$user){
+            \App\Models\Admin::factory()->create([
+                'name' => 'Super Admin',
+                'username' => 'superadmin',
+                'email' => '',
+                'password' => Hash::make('password0987'),
+            ])->assignRole('super-admin');
+        }
+        
 
-        \App\Models\Admin::factory()->create()->assignRole('manager');
+        // \App\Models\Admin::factory()->create()->assignRole('manager');
 
     }
 }
--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -20,17 +20,29 @@ class RoleAndPermissionSeeder extends Seeder
 
         // create permissions
         // Create a permission for the superadmin role:
+            //check if permission exists
+        if(!Permission::where('name', 'can edit')->first())
             Permission::create(['guard_name' => 'admin', 'name' => 'can edit']);
+        if(!Permission::where('name', 'can create')->first())
             Permission::create(['guard_name' => 'admin', 'name' => 'can create']);
+        if(!Permission::where('name', 'can delete')->first())
             Permission::create(['guard_name' => 'admin', 'name' => 'can delete']);
+        if(!Permission::where('name', 'can view')->first())
             Permission::create(['guard_name' => 'admin', 'name' => 'can view']);
 
         // Create a superadmin role for users authenticating with the admin guard:
-        $superadminRole = Role::create(['guard_name' => 'admin', 'name' => 'super-admin'])
-            ->syncPermissions(['can edit', 'can create', 'can delete', 'can view']);
-
-        $managerRole = Role::create(['guard_name' => 'admin', 'name' => 'manager'])
+        // check if role exists
+        if(!Role::where('name', 'super-admin')->first()){
+            $superadminRole = Role::create(['guard_name' => 'admin', 'name' => 'super-admin']);
+            $superadminRole->syncPermissions(['can edit', 'can create', 'can delete', 'can view']);
+        }
+        // $superadminRole = Role::create(['guard_name' => 'admin', 'name' => 'super-admin'])
+        //     ->syncPermissions(['can edit', 'can create', 'can delete', 'can view']);
+        if(!Role::where('name', 'manager')->first()){
+            $managerRole = Role::create(['guard_name' => 'admin', 'name' => 'manager'])
             ->syncPermissions(['can create', 'can view']);
+        }
+        
 
     
 
