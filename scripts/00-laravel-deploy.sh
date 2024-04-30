fec2cdffb06b7d3f0f145fd6563f6e0f4d240fc8
7j4n1 <ahmaholy@gmail.com>
2024-04-30 23:32:47 +0100

    Fix migration command in 00-laravel-deploy.sh script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,6 +2,9 @@
 echo 'Running composer'
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+
+echo 'Clearing all caches...'
+php artisan optimize:clear
  
 echo 'Caching config...'
 php artisan config:cache
@@ -10,7 +13,7 @@ echo 'Caching routes...'
 php artisan route:cache
  
 echo 'Running migrations...'
-php artisan migrate:fresh --seeder="RoleAndPermissionSeeder" --force
+php artisan migrate --seeder="RoleAndPermissionSeeder" --force
 
 echo 'Running seeding...'
 php artisan migrate --seed  --force
