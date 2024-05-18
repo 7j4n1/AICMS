d2d0ddf0b41de605d92669a68453b2a323617a10
7j4n1 <ahmaholy@gmail.com>
2024-05-18 16:41:42 +0100

    feat: Set default string length for database schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
+        Schema::defaultStringLength(191);
         if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
         }
