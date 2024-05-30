df272ad9005e23d2946754b5885f630db0c4fa05
7j4n1 <ahmaholy@gmail.com>
2024-05-30 04:59:49 +0100

    chore: Update auth.php to use 'admin' guard and 'admins' passwords for default authentication

--- config/auth.php
+++ config/auth.php
@@ -14,8 +14,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
-        'passwords' => 'users',
+        'guard' => 'admin',
+        'passwords' => 'admins',
     ],
 
     /*
@@ -102,6 +102,12 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
+            'table' => 'password_reset_tokens',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+        'admins' => [
+            'provider' => 'admins',
             'table' => 'password_reset_tokens',
             'expire' => 60,
             'throttle' => 60,
