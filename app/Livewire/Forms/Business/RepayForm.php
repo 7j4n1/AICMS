68aad2fb0dcb70c4a9a46bf31e27834e9e469e58
7j4n1 <ahmaholy@gmail.com>
2024-06-02 00:04:50 +0100

    feat: Add validation and saving functionality to RepayForm

--- app/Livewire/Forms/Business/RepayForm.php
+++ app/Livewire/Forms/Business/RepayForm.php
@@ -2,10 +2,95 @@
 
 namespace App\Livewire\Forms\Business;
 
-use Livewire\Attributes\Validate;
 use Livewire\Form;
+use NumberFormatter;
+use App\Models\RepayCapture;
+use Livewire\Attributes\Locked;
+use Livewire\Attributes\Validate;
 
 class RepayForm extends Form
 {
-    //
+    #[Locked]
+    public $id;
+    #[Validate('required|number|min:1|exists:members,coopId')]
+    public $coopId;
+    #[Validate('required|number|min:1|exists:item_captures,id')]
+    public $item_capture_id;
+    #[Validate('required')]
+    public $amountToRepay;
+    #[Validate('required|date')]
+    public $repaymentDate;
+    #[Validate('required|numeric|min:0|max:99999999999.99')]
+    public $serviceCharge=0;
+
+    public function boot()
+    {
+        $this->withValidator(function ($validator) {
+            $validator->after(function ($validator) {
+                $item = \App\Models\ItemCapture::find($this->item_capture_id);
+
+                // check if amountToRepay is valid 
+                $result = $this->convertToPhpNumber($this->amountToRepay);
+                if ($result === false) {
+                    $validator->errors()->add('amountToRepay', 'The amount to repay must be a valid number.');
+                }else {
+                    if($item->loanBalance < $result){
+                        $validator->errors()->add('amountToRepay', 'The amount to repay must not be more than the balance.');
+                    }
+                    
+                    if ($result < 100) {
+                        $validator->errors()->add('amountToRepay', 'The amount to repay must be at least 100.');
+                    }
+                }
+                
+            });
+        });
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        // Save to the database
+        try {
+            $repayment = RepayCapture::create([
+                'coopId' => $this->coopId,
+                'item_capture_id' => $this->item_capture_id,
+                'amountToRepay' => $this->amountToRepay,
+                'repaymentDate' => $this->repaymentDate,
+                'serviceCharge' => $this->serviceCharge,
+                'userId' => auth('admin')->user()->id,
+            ]);
+
+            if($repayment){
+                $repayment->updateLoanBalance();
+            }
+
+            return $repayment;
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+
+    public function resetForm()
+    {
+        $this->coopId = '';
+        $this->item_capture_id = '';
+        $this->amountToRepay = 0;
+        $this->repaymentDate = date('Y-m-d');
+        $this->serviceCharge = 0.00;
+    }
+
+    /**
+     * Convert a number from en-US locale to PHP number
+     *
+     * @param string $number
+     * @return float|false
+     */
+    public function convertToPhpNumber($number)
+    {
+        $fmt = new NumberFormatter( 'en_US', NumberFormatter::DECIMAL );
+
+        return $fmt->parse($number, NumberFormatter::TYPE_DOUBLE);
+    }
 }
--- app/Models/RepayCapture.php
+++ app/Models/RepayCapture.php
@@ -21,4 +21,22 @@ class RepayCapture extends Model
         'serviceCharge',
         'userId'
     ];
+
+    public function itemCapture()
+    {
+        return $this->belongsTo(ItemCapture::class,'item_capture_id');
+    }
+
+    // update the loan balance of the item capture
+    public function updateLoanBalance()
+    {
+        $itemCapture = $this->itemCapture;
+        $itemCapture->loanBalance -= (float)$this->amountToRepay;
+        // update the payment status to inactive(0) if the loan balance is 0
+        if($itemCapture->loanBalance == 0){
+            $itemCapture->payment_status = 0;
+        }
+        $itemCapture->loanPaid += (float)$this->amountToRepay;
+        $itemCapture->save();
+    }
 }
